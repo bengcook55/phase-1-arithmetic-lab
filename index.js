39ec69499dcd61314d10fe7b3bac6a4df1efb52b
Benjamin Cook <bengcook55@gmail.com>
2022-11-02 23:39:59 -0400

    commit tree 7e1fe34d3a447b8ab7d22410449d6f7dd486c2f6

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+const multiply = num1 * num2
+
+
+const random = Math.floor(Math.random() * 10) + 1
+
+const num3 = 22
+const num4 = 18
+
+const mod = num3%num4
+
+const max = Math.max(1, 20)
